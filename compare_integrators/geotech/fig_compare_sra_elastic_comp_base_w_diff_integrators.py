Fixed issue where wrong formula was used for youngs modulus for elastic analyses.

compare_integrators/geotech/fig_compare_sra_elastic_comp_base_w_diff_integrators.py
@@ -32,6 +32,7 @@ def site_response(sp, asig, freqs=(0.5, 10), xi=0.03, dy=0.5, analysis_time=None
     osi = o3.OpenSeesInstance(ndm=2, ndf=2, state=3)
     assert isinstance(sp, sm.SoilProfile)
     sp.gen_split(props=['shear_vel', 'unit_mass'], target=dy)
+    req_dt = min(sp.split["thickness"] / sp.split['shear_vel']) / 8
     thicknesses = sp.split["thickness"]
     n_node_rows = len(thicknesses) + 1
     node_depths = np.cumsum(sp.split["thickness"])
@@ -80,12 +81,12 @@ def site_response(sp, asig, freqs=(0.5, 10), xi=0.03, dy=0.5, analysis_time=None
         sl = sp.layer(sl_id)
         app2mod = {}
         if y_depth > sp.gwl:
-            umass = sl.unit_sat_mass / 1e3
+            umass = sl.unit_sat_mass / forder
         else:
-            umass = sl.unit_dry_mass / 1e3
+            umass = sl.unit_dry_mass / forder
         # Define material
         sl_class = o3.nd_material.ElasticIsotropic
-        sl.e_mod = 2 * sl.g_mod * (1 - sl.poissons_ratio) / 1e3
+        sl.e_mod = 2 * sl.g_mod * (1 + sl.poissons_ratio) / forder
         app2mod['rho'] = 'unit_moist_mass'
         overrides = {'nu': sl.poissons_ratio, 'unit_moist_mass': umass}
 
@@ -112,8 +113,6 @@ def site_response(sp, asig, freqs=(0.5, 10), xi=0.03, dy=0.5, analysis_time=None
         # def element
         nodes = [sn[i+1][0], sn[i+1][1], sn[i][1], sn[i][0]]  # anti-clockwise
         eles.append(o3.element.SSPquad(osi, nodes, mat, o3.cc.PLANE_STRAIN, ele_thick, 0.0, -grav))
-
-
 
     # Static analysis
     o3.constraints.Transformation(osi)
@@ -143,9 +142,14 @@ def site_response(sp, asig, freqs=(0.5, 10), xi=0.03, dy=0.5, analysis_time=None
 
     # define material and element for viscous dampers
     base_sl = sp.layer(sp.n_layers)
-    c_base = ele_width * base_sl.unit_dry_mass / 1e3 * sp.get_shear_vel_at_depth(sp.height)
+    c_base = ele_width * base_sl.unit_dry_mass / forder * sp.get_shear_vel_at_depth(sp.height)
     dashpot_mat = o3.uniaxial_material.Viscous(osi, c_base, alpha=1.)
     o3.element.ZeroLength(osi, [dashpot_node_l, dashpot_node_2], mats=[dashpot_mat], dirs=[o3.cc.DOF2D_X])
+    # m_eff = base_sl.unit_dry_mass / forder * thicknesses[-1] * ele_width
+    # k_spring = 4 * m_eff / req_dt ** 2 / 1e3
+    # print('k_spring: ', k_spring)
+    # false_spring = o3.uniaxial_material.Elastic(osi, k_spring)
+    # o3.element.ZeroLength(osi, [dashpot_node_l, dashpot_node_2], mats=[false_spring], dirs=[o3.cc.DOF2D_X])
 
     ods = {}
     for otype in outs:
@@ -261,7 +265,7 @@ def run():
     vs = 150.
     unit_mass = 1700.0
     sl.g_mod = vs ** 2 * unit_mass
-    sl.poissons_ratio = 0.0  # Note that this does not work when v>0
+    sl.poissons_ratio = 0.3
     sl.unit_dry_weight = unit_mass * 9.8
     sl.xi = xi  # for linear analysis
     soil_profile.add_layer(0, sl)
@@ -304,7 +308,7 @@ def run():
     ls = ['-', '--', ':', '-.']
 
     for i, etype in enumerate(etypes):
-        outputs_exp = site_response(soil_profile, in_sig, freqs=(0.5, 10), xi=xi, etype=etype)
+        outputs_exp = site_response(soil_profile, in_sig, freqs=(0.5, 10), xi=xi, etype=etype, forder=1e3)
         resp_dt = (outputs_exp['TIME'][-1] - outputs_exp['TIME'][0]) / (len(outputs_exp['TIME']) - 1)
         surf_sig = eqsig.AccSignal(outputs_exp['ACCX'][0], resp_dt)
         # if etype == 'central_difference':

compare_integrators/geotech/fig_compare_sra_elastic_w_diff_integrators.py
@@ -31,26 +31,32 @@ def site_response(sp, asig, freqs=(0.5, 10), xi=0.03, dy=0.5, analysis_time=None
 
     osi = o3.OpenSeesInstance(ndm=2, ndf=2, state=3)
     assert isinstance(sp, sm.SoilProfile)
-    sp.gen_split(props=['shear_vel', 'unit_mass'], target=dy)
+    sp.gen_split(props=['shear_vel', 'unit_mass', 'g_mod', 'poissons_ratio'], target=dy)
     thicknesses = sp.split["thickness"]
     n_node_rows = len(thicknesses) + 1
     node_depths = np.cumsum(sp.split["thickness"])
     node_depths = np.insert(node_depths, 0, 0)
     ele_depths = (node_depths[1:] + node_depths[:-1]) / 2
-    unit_masses = sp.split["unit_mass"] / forder
-
+    rho = sp.split['unit_mass']
+    g_mod = sp.split['g_mod']
+    poi = sp.split['poissons_ratio']
+    lam = 2 * g_mod * poi / (1 - 2 * poi)
+    mu = g_mod
+    v_dil = np.sqrt((lam + 2 * mu) / rho)
+    ele_h = sp.split['thickness']
+    dts = ele_h / v_dil
+    min_dt = min(dts)
+    print('min_dt: ', min_dt)
     grav = 9.81
 
     ele_width = min(thicknesses)
-    total_soil_nodes = len(thicknesses) * 2 + 2
 
     # Define nodes and set boundary conditions for simple shear deformation
     # Start at top and build down?
     sn = [[o3.node.Node(osi, 0, 0), o3.node.Node(osi, ele_width, 0)]]
     for i in range(1, n_node_rows):
         # Establish left and right nodes
-        sn.append([o3.node.Node(osi, 0, -node_depths[i]),
-                    o3.node.Node(osi, ele_width, -node_depths[i])])
+        sn.append([o3.node.Node(osi, 0, -node_depths[i]), o3.node.Node(osi, ele_width, -node_depths[i])])
         # set x and y dofs equal for left and right nodes
         o3.EqualDOF(osi, sn[i][0], sn[i][1], [o3.cc.X, o3.cc.Y])
 
@@ -77,7 +83,7 @@ def site_response(sp, asig, freqs=(0.5, 10), xi=0.03, dy=0.5, analysis_time=None
             umass = sl.unit_dry_mass / 1e3
         # Define material
         sl_class = o3.nd_material.ElasticIsotropic
-        sl.e_mod = 2 * sl.g_mod * (1 - sl.poissons_ratio) / 1e3
+        sl.e_mod = 2 * sl.g_mod * (1 + sl.poissons_ratio) / 1e3
         app2mod['rho'] = 'unit_moist_mass'
         overrides = {'nu': sl.poissons_ratio, 'unit_moist_mass': umass}
 
@@ -153,10 +159,10 @@ def site_response(sp, asig, freqs=(0.5, 10), xi=0.03, dy=0.5, analysis_time=None
                 for i in range(len(outs['STRS'])):
                     ind = np.argmin(abs(ele_depths - outs['STRS'][i]))
                     ods['STRS'].append(o3.recorder.ElementToArrayCache(osi, ele=eles[ind], arg_vals=['strain'], dt=rec_dt))
-
+    ods['TIME'] = o3.recorder.TimeToArrayCache(osi, dt=rec_dt)
     # Define the dynamic analysis
 
-    n = 4
+    n = 8
     # omegas = np.array(o3.get_eigen(osi, solver='fullGenLapack', n=n)) ** 0.5  # DO NOT USE fullGenLapack
     omegas = np.array(o3.get_eigen(osi, n=n)) ** 0.5
     periods = 2 * np.pi / omegas
@@ -178,20 +184,20 @@ def site_response(sp, asig, freqs=(0.5, 10), xi=0.03, dy=0.5, analysis_time=None
         if etype == 'newmark_explicit':
             o3.system.ProfileSPD(osi)
             o3.integrator.NewmarkExplicit(osi, gamma=0.5)
-            explicit_dt = periods[-1] / np.pi / 32
+            # explicit_dt = periods[-1] / np.pi / 32
+            explicit_dt = min_dt / 10
         elif etype == 'central_difference':
             o3.system.ProfileSPD(osi)
             o3.integrator.CentralDifference(osi)
-            explicit_dt = periods[-1] / np.pi / 32
-        elif etype == 'hht_explicit':
-            o3.integrator.HHTExplicit(osi, alpha=0.5)
-            explicit_dt = periods[-1] / np.pi / 8
+            # explicit_dt = periods[-1] / np.pi / 32
+            explicit_dt = min_dt / 10
         elif etype == 'explicit_difference':
             # o3.opy.system('Diagonal')
             # o3.system.FullGeneral(osi)
             o3.system.Diagonal(osi)
             o3.integrator.ExplicitDifference(osi)
-            explicit_dt = periods[-1] / np.pi / 32
+            # explicit_dt = periods[-1] / np.pi / 32
+            explicit_dt = min_dt / 10
         else:
             raise ValueError(etype)
         print('explicit_dt: ', explicit_dt)
@@ -232,7 +238,7 @@ def site_response(sp, asig, freqs=(0.5, 10), xi=0.03, dy=0.5, analysis_time=None
             out_dict[otype] = np.array(out_dict[otype])
         else:
             out_dict[otype] = ods[otype].collect().T
-    out_dict['time'] = np.arange(0, analysis_time, dt)
+    # out_dict['time'] = np.arange(0, analysis_time, dt)
 
     return out_dict
 
@@ -246,7 +252,7 @@ def run():
     vs = 250.
     unit_mass = 1700.0
     sl.g_mod = vs ** 2 * unit_mass
-    sl.poissons_ratio = 0.0  # Note that this does not work when v>0
+    sl.poissons_ratio = 0.3  # Note that this does not work when v>0
     sl.unit_dry_weight = unit_mass * 9.8
     sl.xi = xi  # for linear analysis
     soil_profile.add_layer(0, sl)
@@ -255,7 +261,7 @@ def run():
     vs = 350.
     unit_mass = 1700.0
     sl_base.g_mod = vs ** 2 * unit_mass
-    sl_base.poissons_ratio = 0.0
+    sl_base.poissons_ratio = 0.3
     sl_base.unit_dry_weight = unit_mass * 9.8
     sl_base.xi = xi  # for linear analysis
     soil_profile.add_layer(19., sl_base)
@@ -288,8 +294,8 @@ def run():
     ls = ['-', '--', ':', '-.']
 
     for i, etype in enumerate(etypes):
-        outputs_exp = site_response(soil_profile, in_sig, freqs=(0.5, 10), xi=xi, etype=etype)
-        resp_dt = (outputs_exp['time'][-1] - outputs_exp['time'][0]) / (len(outputs_exp['time']) - 1)
+        outputs_exp = site_response(soil_profile, in_sig, freqs=(0.5, 10), xi=xi, etype=etype, rec_dt=0.01)
+        resp_dt = (outputs_exp['TIME'][-1] - outputs_exp['TIME'][0]) / (len(outputs_exp['TIME']) - 1)
         surf_sig = eqsig.AccSignal(outputs_exp['ACCX'][0], resp_dt)
         surf_sig.smooth_fa_frequencies = in_sig.fa_frequencies[1:]
         sps[0].plot(surf_sig.time, surf_sig.values, c=cbox(i), label=etype, ls=ls[i])